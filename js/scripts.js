b808c74df5a9acfc8c0b3751904ee7f91c1b533a
Alibimba <ehot2903@mail.ru>
2022-05-06 11:45:25 +0300

    Траты и ЗП работате еееее v0.2.3

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,20 @@ function sum(odin, dva, tri, input) {
     console.log(a + '+' + b + '=' + (a + b));
     input.innerHTML = d;
 }
+function sumT(odinT, dvaT, triT, inputT) {
+    let f = Number(odinT) + Number(dvaT) + Number(triT);
+    console.log(a + '+' + b + '=' + (a + b));
+    inputT.innerHTML = f;
+}
 
 let odin = document.querySelector('#o');
+let odinT = document.querySelector('#a');
 let dva = document.querySelector('#oo');
+let dvaT = document.querySelector('#aa');
 let tri = document.querySelector('#ooo');
+let triT = document.querySelector('#aaa');
 let vivodZP = document.querySelector('#itogo');
+let vivodTRAT = document.querySelector('#itogo1');
 
 let a = Number(odin.value);
 let b = Number(dva.value);
@@ -22,4 +31,13 @@ dva.addEventListener('input', function () {
 })
 tri.addEventListener('input', function () {
     sum(odin.value, dva.value, tri.value, vivodZP)
+})
+odinT.addEventListener('input', function () {
+    sumT(odinT.value, dvaT.value, triT.value, vivodTRAT)
+})
+dvaT.addEventListener('input', function () {
+    sumT(odinT.value, dvaT.value, triT.value, vivodTRAT)
+})
+triT.addEventListener('input', function () {
+    sumT(odinT.value, dvaT.value, triT.value, vivodTRAT)
 })
